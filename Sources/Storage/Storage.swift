abdbe8414b82ba9f14292a0e09d67a53b7929fee
Xavier Lowmiller <xaver.lohmueller@me.com>
2022-03-05 07:28:09 +0100

    Async edition

--- Sources/Storage/Storage.swift
+++ Sources/Storage/Storage.swift
@@ -1,5 +1,4 @@
-import Foundation
-import Combine
+@preconcurrency import Foundation
 
 private let encoder = JSONEncoder()
 private let decoder = JSONDecoder()
@@ -9,7 +8,7 @@ public enum StorageError: Error, Equatable, Hashable {
     case storageFailed(_ underlying: Error)
     case deletionFailed(_ underlying: Error)
 
-    public static func == (lhs: StorageError, rhs: StorageError) -> Bool {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.localizedDescription == rhs.localizedDescription
     }
 
@@ -18,13 +17,11 @@ public enum StorageError: Error, Equatable, Hashable {
     }
 }
 
-public struct Storage {
+public actor Storage {
     /// The root directory that files should be saved in
     private let containerURL: URL
 
     private let fileManager: FileManager
-
-    private let queue = DispatchQueue(label: "storage background queue")
 
     /// File-based storage
     ///
@@ -37,9 +34,6 @@ public struct Storage {
     ///  |-- id2.json
     ///  |- SomeOtherEntityType
     ///  |-- id1.json
-    ///  |- CustomFolder
-    ///  |-- SomePrivateEntity
-    ///  |--- id1.json
     ///  ```
     ///
     /// - Parameters:
@@ -59,134 +53,92 @@ public struct Storage {
     /// Loads a stored entity
     /// - Parameters:
     ///   - id: The ID of the entity to load
-    ///   - folder: An optional custom folder to load items in
     /// - Returns: The Entity (if it exists)
-    public func load<T: Codable & Identifiable>(_ id: T.ID, in folder: String? = nil) -> AnyPublisher<T, Error> {
-        Future<T, Error> { promise in
-            queue.async {
-                do {
-                    let url = folderURL(for: T.self)
-                        .appendingOptionalPathComponent(folder)
-                        .appendingPathComponent("\(id).json")
-                    let data = try Data(contentsOf: url)
-                    let instance = try decoder.decode(T.self, from: data)
+    public func load<T: Codable & Identifiable>(_ id: T.ID) async throws -> T {
 
-                    promise(.success(instance))
-                } catch {
-                    promise(.failure(StorageError.loadingFailed(error)))
-                }
-            }
-        }.eraseToAnyPublisher()
+        do {
+            let url = folderURL(for: T.self)
+                .appendingPathComponent("\(id).json")
+            let data = try Data(contentsOf: url)
+            let instance = try decoder.decode(T.self, from: data)
+
+            return instance
+        } catch {
+            throw StorageError.loadingFailed(error)
+        }
     }
 
     /// Loads all entities of a given type
     /// - Parameters:
     ///   - kind: The type of the entities to load
-    ///   - folder: An optional custom folder to load items in
     /// - Returns: All entities of the specified kind
-    public func loadAll<T: Codable & Identifiable>(_ kind: T.Type, in folder: String? = nil) -> AnyPublisher<[T], Error> {
-        Future<[T], Error> { promise in
-            queue.async {
-                do {
-                    let url = folderURL(for: T.self)
-                        .appendingOptionalPathComponent(folder)
-                    guard fileManager.fileExists(atPath: url.path) else { return promise(.success([])) }
-                    let instances = try fileManager.contentsOfDirectory(
-                        at: url,
-                        includingPropertiesForKeys: nil,
-                        options: []).map { url -> T in
+    public func loadAll<T: Codable & Identifiable>(_ kind: T.Type) async throws -> [T] {
+        do {
+            let url = folderURL(for: T.self)
+            guard fileManager.fileExists(atPath: url.path) else { return [] }
+            let instances = try fileManager.contentsOfDirectory(
+                at: url,
+                includingPropertiesForKeys: nil,
+                options: []).map { url -> T in
 
-                            let data = try Data(contentsOf: url)
-                            return try decoder.decode(T.self, from: data)
-                        }
-                    promise(.success(instances))
-                } catch {
-                    promise(.failure(StorageError.loadingFailed(error)))
+                    let data = try Data(contentsOf: url)
+                    return try decoder.decode(T.self, from: data)
                 }
-            }
-        }.eraseToAnyPublisher()
+            return instances
+        } catch {
+            throw StorageError.loadingFailed(error)
+        }
     }
 
     /// Store an entity
     /// - Parameters:
     ///   - entity: The entity to store
-    ///   - folder: An optional custome folder to store the entity in
     /// - Returns: Nothing on success, or any error
-    public func store<T: Codable & Identifiable>(entity: T, in folder: String? = nil) -> AnyPublisher<Void, Error> {
-        Future<Void, Error> { promise in
-            queue.async {
-                do {
-                    let data = try encoder.encode(entity)
-                    let url = folderURL(for: T.self)
-                        .appendingOptionalPathComponent(folder)
-                        .appendingPathComponent("\(entity.id).json")
-                    try? fileManager.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
-                    try data.write(to: url, options: .atomicWrite)
-                    promise(.success(()))
-                } catch {
-                    promise(.failure(StorageError.storageFailed(error)))
-                }
-            }
-        }.eraseToAnyPublisher()
+    public func store<T: Codable & Identifiable>(entity: T) async throws {
+        do {
+            let data = try encoder.encode(entity)
+            let url = folderURL(for: T.self)
+                .appendingPathComponent("\(entity.id).json")
+            try? fileManager.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
+            try data.write(to: url, options: .atomicWrite)
+        } catch {
+            throw StorageError.storageFailed(error)
+        }
     }
 
     /// Deletes a given entity
     /// - Parameters:
     ///   - entity: The entity to delete
-    ///   - folder: An optional custom folder to delete the entity in
     /// - Returns: Nothing on success, or any error
-    public func delete<T: Codable & Identifiable>(_ entity: T, in folder: String? = nil) -> AnyPublisher<Void, Error> {
-        Future<Void, Error> { promise in
-            queue.async {
-                do {
-                    let url = folderURL(for: T.self)
-                        .appendingOptionalPathComponent(folder)
-                        .appendingPathComponent("\(entity.id).json")
-                    if fileManager.fileExists(atPath: url.path) {
-                        try fileManager.removeItem(at: url)
-                    }
-                    promise(.success(()))
-                } catch {
-                    promise(.failure(StorageError.deletionFailed(error)))
-                }
+    public func delete<T: Codable & Identifiable>(_ entity: T) async throws {
+        do {
+            let url = folderURL(for: T.self)
+                .appendingPathComponent("\(entity.id).json")
+            if fileManager.fileExists(atPath: url.path) {
+                try fileManager.removeItem(at: url)
             }
-        }.eraseToAnyPublisher()
+        } catch {
+            throw StorageError.deletionFailed(error)
+        }
     }
 
     /// Deletes all entities of a given type
     /// - Parameters:
     ///   - kind: The type of the entities to delete
-    ///   - folder: An optional custom folder to delete entities in
     /// - Returns: Nothing on success, or any error
-    public func deleteAll<T: Codable & Identifiable>(_ kind: T.Type, in folder: String? = nil) -> AnyPublisher<Void, Error> {
-        Future<Void, Error> { promise in
-            queue.async {
-                do {
-                    let url = folderURL(for: T.self)
-                        .appendingOptionalPathComponent(folder)
-                    try fileManager.contentsOfDirectory(
-                        at: url,
-                        includingPropertiesForKeys: nil,
-                        options: []).forEach { url in
-                            if fileManager.fileExists(atPath: url.path) {
-                                try fileManager.removeItem(at: url)
-                            }
-                        }
-                    promise(.success(()))
-                } catch {
-                    promise(.failure(StorageError.deletionFailed(error)))
+    public func deleteAll<T: Codable & Identifiable>(_ kind: T.Type) async throws {
+        do {
+            let url = folderURL(for: T.self)
+            try fileManager.contentsOfDirectory(
+                at: url,
+                includingPropertiesForKeys: nil,
+                options: []).forEach { url in
+                    if fileManager.fileExists(atPath: url.path) {
+                        try fileManager.removeItem(at: url)
+                    }
                 }
-            }
-        }.eraseToAnyPublisher()
-    }
-}
-
-private extension URL {
-    func appendingOptionalPathComponent(_ component: String?) -> URL {
-        if let component = component {
-            return self.appendingPathComponent(component)
-        } else {
-            return self
+        } catch {
+            throw StorageError.deletionFailed(error)
         }
     }
 }
--- Tests/StorageTests/StorageTests.swift
+++ Tests/StorageTests/StorageTests.swift
@@ -1,5 +1,4 @@
-import XCTest
-import Combine
+@preconcurrency import XCTest
 @testable import Storage
 
 struct Entity: Codable, Hashable, Identifiable {
@@ -13,256 +12,80 @@ final class StorageTests: XCTestCase {
     private let testEntity2 = Entity(id: 2, data: "data 2")
     private let testEntity3 = Entity(id: 3, data: "data 3")
 
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
+    override func tearDown() async throws {
         try tempFile.deleteDirectory()
+        try await super.tearDown()
     }
 
-    func testStoringAndLoadingTheSameEntityShouldRestoreItIdentically() throws {
+    func testStoringAndLoadingTheSameEntityShouldRestoreItIdentically() async throws {
         // Given
         let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
 
         // When
-        let token = storage.store(entity: testEntity1)
-            .flatMap { storage.load(self.testEntity1.id) }
-            .sink { result in
-                result.assertSuccess()
-                exp.fulfill()
-            } receiveValue: {
-                // Then
-                XCTAssertEqual($0, self.testEntity1)
-            }
+        try await storage.store(entity: testEntity1)
+        let value: Entity = try await storage.load(self.testEntity1.id)
 
-        wait(for: [exp], timeout: 1)
-        token.cancel()
+        // Then
+        XCTAssertEqual(value, self.testEntity1)
     }
 
-    func testLoadingANonExistingEntityShouldFail() throws {
+    func testLoadingANonExistingEntityShouldFail() async throws {
         // Given
         let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
 
-        // When
-        let token = storage.load(self.testEntity1.id)
-            .sink { result in
-                // Then
-                result.assertFailure()
-                exp.fulfill()
-            } receiveValue: { (result: Entity) in
-                XCTFail("Shouldn't load an entity that doesn't exist")
-            }
-
-        wait(for: [exp], timeout: 1)
-        token.cancel()
+        // Then
+        await XCTAssertThrowsError(try await storage.load(self.testEntity1.id) as Entity)
     }
 
-    func testStoringAndDeletingShouldRemoveTheEntity() {
+    func testStoringAndDeletingShouldRemoveTheEntity() async throws {
         // Given
         let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
 
         // When
-        let token = storage.store(entity: testEntity1)
-            .flatMap { storage.delete(self.testEntity1) }
-            .flatMap { storage.loadAll(Entity.self) }
-            .sink(receiveCompletion: { result in
-                result.assertSuccess()
-                exp.fulfill()
-            }, receiveValue: { entities in
-                XCTAssert(entities.isEmpty)
-            })
-
-        wait(for: [exp], timeout: 1)
-        token.cancel()
+        try await storage.store(entity: testEntity1)
+        try await storage.delete(self.testEntity1)
+        let entities = try await storage.loadAll(Entity.self)
+        // Then
+        XCTAssert(entities.isEmpty)
     }
 
-    func testDeletingANonExistingEntityShouldSignalSuccess() throws {
+    func testDeletingANonExistingEntityShouldSignalSuccess() async throws {
         // Given
         let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
 
         // When
-        let token = storage.delete(self.testEntity1)
-            .sink { result in
-                // Then
-                result.assertSuccess()
-                exp.fulfill()
-            } receiveValue: {
-                // Void should be received
-            }
+        try await storage.delete(self.testEntity1)
 
-        wait(for: [exp], timeout: 1)
-        token.cancel()
+        // Then
+        // Pass! No error was thrown
     }
 
-    func testLoadAllShouldLoadAllEntitiesOfAKind() {
+    func testLoadAllShouldLoadAllEntitiesOfAKind() async throws {
         // Given
         let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
 
         // When
-        let token = storage.store(entity: testEntity1)
-            .flatMap { storage.store(entity: self.testEntity2) }
-            .flatMap { storage.loadAll(Entity.self) }
-            .sink(receiveCompletion: { result in
-                result.assertSuccess()
-                exp.fulfill()
-            }, receiveValue: { entities in
-                XCTAssertEqual(entities.count, 2)
-            })
+        try await storage.store(entity: testEntity1)
+        try await storage.store(entity: self.testEntity2)
+        let entities = try await storage.loadAll(Entity.self)
 
-        wait(for: [exp], timeout: 1)
-        token.cancel()
+        // Then
+        XCTAssertEqual(entities.count, 2)
     }
 
-    func testDeleteAllShouldDeleteAllEntitiesOfAKind() {
+    func testDeleteAllShouldDeleteAllEntitiesOfAKind() async throws {
         // Given
         let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
+        try await storage.store(entity: testEntity1)
+        try await storage.store(entity: self.testEntity2)
+        let entitiesBeforeDeletion = try await storage.loadAll(Entity.self)
+        XCTAssertEqual(entitiesBeforeDeletion.count, 2)
 
         // When
-        let token = storage.store(entity: testEntity1)
-            .flatMap { storage.store(entity: self.testEntity2) }
-            .flatMap { storage.loadAll(Entity.self) }
-            .handleEvents(receiveOutput: {
-                XCTAssertEqual($0.count, 2)
-            })
-            .flatMap { _ in storage.deleteAll(Entity.self) }
-            .flatMap { storage.loadAll(Entity.self) }
-            .sink(receiveCompletion: { result in
-                result.assertSuccess()
-                exp.fulfill()
-            }, receiveValue: { entities in
-                XCTAssert(entities.isEmpty)
-            })
+        try await storage.deleteAll(Entity.self)
+        let entitiesAfterDeletion = try await storage.loadAll(Entity.self)
 
-        wait(for: [exp], timeout: 1)
-        token.cancel()
-    }
-
-    // MARK: - Folder Tests
-
-    func testStoringAndLoadingInFolder() {
-        // Given
-        let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
-
-        // When
-        let token = storage.store(entity: testEntity1, in: "user1")
-            .flatMap { storage.load(self.testEntity1.id, in: "user1") }
-            .sink { result in
-                result.assertSuccess()
-                exp.fulfill()
-            } receiveValue: {
-                // Then
-                XCTAssertEqual($0, self.testEntity1)
-            }
-
-        wait(for: [exp], timeout: 1)
-        token.cancel()
-    }
-
-    func testStoringAndLoadingInDifferentFolder() {
-        // Given
-        let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
-
-        // When
-        let token = storage.store(entity: testEntity1, in: "user1")
-            .flatMap { storage.load(self.testEntity1.id, in: "user2") }
-            .sink { result in
-                result.assertFailure()
-                exp.fulfill()
-            } receiveValue: { (result: Entity) in
-                // Then
-                XCTFail("Shouldn't load contents of different folder")
-            }
-
-        wait(for: [exp], timeout: 1)
-        token.cancel()
-    }
-
-    func testStoringAndDeletingShouldRemoveTheEntityInsideFolders() {
-        // Given
-        let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
-
-        // When
-        let token = storage.store(entity: testEntity1, in: "user1")
-            .flatMap { storage.delete(self.testEntity1, in: "user1") }
-            .flatMap { storage.loadAll(Entity.self, in: "user1") }
-            .sink(receiveCompletion: { result in
-                result.assertSuccess()
-                exp.fulfill()
-            }, receiveValue: { entities in
-                XCTAssert(entities.isEmpty)
-            })
-
-        wait(for: [exp], timeout: 1)
-        token.cancel()
-    }
-
-    func testStoringAndDeletingShouldNotTouchTheEntityInDifferentFolders() {
-        // Given
-        let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
-
-        // When
-        let token = storage.store(entity: testEntity1, in: "user1")
-            .flatMap { storage.delete(self.testEntity1, in: "user2") }
-            .flatMap { storage.loadAll(Entity.self, in: "user1") }
-            .sink(receiveCompletion: { result in
-                result.assertSuccess()
-                exp.fulfill()
-            }, receiveValue: { entities in
-                XCTAssertEqual(entities, [self.testEntity1])
-            })
-
-        wait(for: [exp], timeout: 1)
-        token.cancel()
-    }
-
-    func testLoadAllShouldLoadAllEntitiesOfAKindInAFolder() {
-        // Given
-        let storage = Storage(containerURL: tempFile.directoryURL)
-        let exp = XCTestExpectation(description: #function)
-
-        // When
-        let token = storage.store(entity: testEntity1, in: "user1")
-            .flatMap { storage.store(entity: self.testEntity2, in: "user2") }
-            .flatMap { storage.store(entity: self.testEntity3, in: "user1") }
-            .flatMap { storage.loadAll(Entity.self, in: "user1") }
-            .sink(receiveCompletion: { result in
-                result.assertSuccess()
-                exp.fulfill()
-            }, receiveValue: { entities in
-                XCTAssertEqual(entities, [self.testEntity1, self.testEntity3])
-            })
-
-        wait(for: [exp], timeout: 1)
-        token.cancel()
-    }
-}
-
-extension Subscribers.Completion {
-    func assertSuccess() {
-        switch self {
-        case .finished:
-            // Pass!
-            break
-        case .failure(let error):
-            XCTFail(error.localizedDescription)
-        }
-    }
-
-    func assertFailure() {
-        switch self {
-        case .finished:
-            XCTFail("Unexpectedly, the operation succeeded")
-        case .failure:
-            // Pass!
-            break
-        }
+        // Then
+        XCTAssert(entitiesAfterDeletion.isEmpty)
     }
 }
